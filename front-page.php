aeda6f7bdae8c924a0d05fe89f2a1304338e13d5
satrain <mladen.trickovic5@gmail.com>
2023-10-09 17:54:56 +0200

    hero section frontend

--- front-page.php
+++ front-page.php
@@ -1,5 +1,27 @@
 <?php get_header() ?>
 
+<div class="hero homepage-hero">
+    <div class="sliding-text-container">
+        <p class="sliding-text">distinctive©aestheticdistinctive©aestheticdistinctive©aestheticdistinctive©aesthetic</p>
+    </div>
+    <div class="container homepage-hero-container">
+        <div class="sliding-text-navigation">
+            <p>Masterclass by Danilo&Sharon</p>
+            <p>Working worldwide</p>
+        </div>
+
+        <div class="hero-content">
+            <div class="hero-copy">
+                <h2>Our masterclass will make the path of your flash mastery quick and easy.</h2>
+                <a href="#" class="btn btn-secondary">Enroll now ↗</a>
+            </div>
+            <div class="hero-mock">
+                <img src="/wp-content/uploads/2023/10/hero-main-image.png" alt="Hero image">
+            </div>
+        </div>
+    </div>
+</div>
+
 <div class="money-back-guarantee">
     <div class="container money-back-guarantee-container">
         <a class="btn btn-primary">21 Day refund policy</a>
