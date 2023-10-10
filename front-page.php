759c86f42d5b36acec061e845bae6c912fe2c4b2
satrain <mladen.trickovic5@gmail.com>
2023-10-10 17:24:54 +0200

    our story, online lessons full frontend done, video-n-accordion block frontend structure

--- front-page.php
+++ front-page.php
@@ -57,6 +57,70 @@
     </div>
 </div>
 
+<div class="our-story">
+    <div class="container our-story-container">
+        <h2>DANILO&SHARON Our story</h2>
+        <p>Working internationally, Danilo and Sharon photographed weddings and portraits around the world, from the busy streets of Hong Kong to historic Paris.</p>
+
+        <div class="our-story-list">
+            <div class="as-featured-in">
+                <a class="btn btn-primary">As featured in</a>
+                <p>VOGUE ∙ BRITISH VOGUE ∙ ELLE ∙ ELLE DECOR∙ VANITY FAIR ∙ THE LANE ∙ BRIDES ∙ JUNEBUG ∙ COSMOPOLITAN ∙ RANGEFINDER MAGAZINE ∙ THE WED ∙ DAILY MAIL ∙ HUFFINGTON POST</p>
+            </div>
+            <div class="previous-destinations">
+                <a class="btn btn-primary">Previous destinations</a>
+                <p>ITALY ∙ FRANCE ∙ GREECE ∙SPAIN∙PORTUGAL ∙ SWITZERLAND ∙ GERMANY ∙ BELGIUM ∙ THE NETHERLANDS ∙ AUSTRIA ∙ SWEDEN ∙ FINLAND ∙ CROATIA ∙ MONTENEGRO ∙ HONG KONG</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="online-lessons">
+    <div class="container online-lessons-container">
+        <h2>Interactive
+            <br>
+            <span>online</span>
+            <br>
+            less<img src="<?= get_template_directory_uri() ?>/assets/images/online-lessons-danilo-cta.svg" alt="O">ns</h2>
+        <p>Unlock your photography skills in a real-time & interactive classroom with your peers and your mentor.</p>
+        <a class="btn online-lessons-btn" href="#">Check more about</a>
+    </div>
+</div>
+
+<div class="video-n-accordion">
+    <div class="video-holder"></div>
+    <div class="accordion-holder">
+        <div class="container video-n-accordion-container">
+            <div class="copy-block">
+                <div class="copy-block-left copy-block-text">
+                    <h2>You just need a camera, and a <span>passion for photography</span> and learning.</h2>
+                    <p>Revolutionize photography education globally.</p>
+                </div>
+                <div class="copy-block-right copy-block-accordion">
+                    <div class="accordion-wrapper">
+                        <div class="item">
+                            <p class="title">Startup Phase</p>
+                            <p class="content">Working internationally, Danilo and Sharon photographed weddings and portraits around the world, from the busy streets of Hong Kong to historic Paris.The studio is based in Belgrade and is led by wife and husband - Sharon and Danilo Vasic.</p>
+                        </div>
+                        <div class="item">
+                            <p class="title">Growth Phase</p>
+                            <p class="content">Working internationally, Danilo and Sharon photographed weddings and portraits around the world, from the busy streets of Hong Kong to historic Paris.The studio is based in Belgrade and is led by wife and husband - Sharon and Danilo Vasic.</p>
+                        </div>
+                        <div class="item">
+                            <p class="title">Maturity Phase</p>
+                            <p class="content">Working internationally, Danilo and Sharon photographed weddings and portraits around the world, from the busy streets of Hong Kong to historic Paris.The studio is based in Belgrade and is led by wife and husband - Sharon and Danilo Vasic.</p>
+                        </div>
+                    </div>
+                    <div class="buttons-wrapper">
+                        <a class="btn btn-primary">Enrollment Open</a>
+                        <a class="btn btn-secondary">Check more about</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 <div class="money-back-guarantee">
     <div class="container money-back-guarantee-container">
         <a class="btn btn-primary">21 Day refund policy</a>
