fa44273b61f447720639ea3c4d8339d73430dd4b
satrain <mladen.trickovic5@gmail.com>
2023-10-15 00:04:54 +0200

    finish desktop homepage frontend

--- front-page.php
+++ front-page.php
@@ -88,9 +88,33 @@
 </div>
 
 <div class="video-n-accordion">
-    <div class="video-holder"></div>
-    <div class="accordion-holder">
-        <div class="container video-n-accordion-container">
+    <div class="video-holder">
+        <video width="100%" poster="/wp-content/uploads/2023/10/video-n-accordion-video-thumbnail.jpeg">
+            <source src="/wp-content/uploads/2023/10/video_placeholder.mp4" type="video/mp4">
+        </video>
+        <div class="play-btn" id="playVideo">
+            <svg xmlns="http://www.w3.org/2000/svg" width="248" height="248" viewBox="0 0 248 248" fill="none">
+                <circle cx="124" cy="124" r="123.5" stroke="white"/>
+                <g filter="url(#filter0_d_66_184)">
+                    <path d="M94 72.0385L184 124L94 175.962L94 72.0385Z" stroke="white" shape-rendering="crispEdges"/>
+                </g>
+                <defs>
+                    <filter id="filter0_d_66_184" x="89.5" y="71.1725" width="99.5" height="113.655" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+                        <feOffset dy="4"/>
+                        <feGaussianBlur stdDeviation="2"/>
+                        <feComposite in2="hardAlpha" operator="out"/>
+                        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+                        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_66_184"/>
+                        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_66_184" result="shape"/>
+                    </filter>
+                </defs>
+            </svg>
+        </div>
+    </div>
+    <div class="accordion-holder container">
+        <div class="video-n-accordion-container">
             <div class="copy-block">
                 <div class="copy-block-left copy-block-text">
                     <h2>You just need a camera, and a <span>passion for photography</span> and learning.</h2>
@@ -98,23 +122,167 @@
                 </div>
                 <div class="copy-block-right copy-block-accordion">
                     <div class="accordion-wrapper">
-                        <div class="item">
+                        <div class="item active">
                             <p class="title">Startup Phase</p>
                             <p class="content">Working internationally, Danilo and Sharon photographed weddings and portraits around the world, from the busy streets of Hong Kong to historic Paris.The studio is based in Belgrade and is led by wife and husband - Sharon and Danilo Vasic.</p>
                         </div>
                         <div class="item">
                             <p class="title">Growth Phase</p>
-                            <p class="content">Working internationally, Danilo and Sharon photographed weddings and portraits around the world, from the busy streets of Hong Kong to historic Paris.The studio is based in Belgrade and is led by wife and husband - Sharon and Danilo Vasic.</p>
+                            <!-- <p class="content">Working internationally, Danilo and Sharon photographed weddings and portraits around the world, from the busy streets of Hong Kong to historic Paris.The studio is based in Belgrade and is led by wife and husband - Sharon and Danilo Vasic.</p> -->
                         </div>
                         <div class="item">
                             <p class="title">Maturity Phase</p>
-                            <p class="content">Working internationally, Danilo and Sharon photographed weddings and portraits around the world, from the busy streets of Hong Kong to historic Paris.The studio is based in Belgrade and is led by wife and husband - Sharon and Danilo Vasic.</p>
+                            <!-- <p class="content">Working internationally, Danilo and Sharon photographed weddings and portraits around the world, from the busy streets of Hong Kong to historic Paris.The studio is based in Belgrade and is led by wife and husband - Sharon and Danilo Vasic.</p> -->
                         </div>
                     </div>
                     <div class="buttons-wrapper">
-                        <a class="btn btn-primary">Enrollment Open</a>
-                        <a class="btn btn-secondary">Check more about</a>
+                        <a class="btn btn-primary" href="#">Enrollment Open</a>
+                        <a class="btn btn-secondary" href="#">Check more about</a>
                     </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="courses">
+    <div class="container courses-container">
+        <p class="courses-subtitle">Courses</p>
+        <h2>Our courses will make the path of your flash mastery quick and easy.</h2>
+        <div class="courses-cards-wrapper">
+            <div class="card first-course">
+                <div class="card-background-radial"></div>
+                <div class="card-upper">
+                    <a class="btn btn-primary subtitle">Working with lance</a>
+                    <h3>Wedding photography flash.</h3>
+                </div>
+                <div class="card-lower">
+                    <p>An approachable way to learn web design & CSS in Squarespace, get up to speed on freelancing workflows (hourly rates, client contracts), and level up with 1on1 design critiques!</p>
+                    <div class="buttons-wrapper">
+                        <a class="btn btn-primary">Enrollment Open</a>
+                        <a class="btn btn-primary">Check more about</a>
+                    </div>
+                </div>
+            </div>
+            <div class="card second-course">
+                <div class="card-upper">
+                    <a class="btn btn-primary subtitle">Working with lance</a>
+                    <h3>Lorem ipsum course name comes here.</h3>
+                </div>
+                <div class="card-lower">
+                    <p>An approachable way to learn web design & CSS in Squarespace, get up to speed on freelancing workflows (hourly rates, client contracts), and level up with 1on1 design critiques!</p>
+                    <div class="buttons-wrapper">
+                        <a class="btn btn-primary">Enrollment Open</a>
+                        <a class="btn btn-primary">Check more about</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="real-results">
+    <div class="sliding-text-container">
+        <p class="sliding-text">real people, real results real people, real results real people, real results</p>
+    </div>
+    <div class="container real-results-container">
+        <div class="sliding-text-navigation">
+            <p>Let's see what they have to say</p>
+            <p>Our worldwide clients</p>
+        </div>
+
+        <div class="real-results-wrapper">
+            <div class="item">
+                <div class="image"></div>
+                <a class="btn btn-primary real-results-btn">New York City</a>
+                <p class="name">Magdalene Kennedy</p>
+                <p class="profession">Animator</p>
+            </div>
+            <div class="item">
+                <div class="image"></div>
+                <a class="btn btn-primary real-results-btn">Madrid</a>
+                <p class="name">Megan Toriglia</p>
+                <p class="profession">Photographer</p>
+            </div>
+            <div class="item">
+                <div class="image"></div>
+                <a class="btn btn-primary real-results-btn">Los Angeles</a>
+                <p class="name">Dani Thomason</p>
+                <p class="profession">Photographer / Editor</p>
+            </div>
+            <div class="item">
+                <div class="image"></div>
+                <a class="btn btn-primary real-results-btn">New York City</a>
+                <p class="name">Nicole Mayer</p>
+                <p class="profession">Animator</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="newsletter">
+    <div class="container newsletter-container">
+        <div class="newsletter-copy">
+            <h2>Sign up for our newsletter and get 10% on your first purchase.</h2>
+            <form id="newsletterForm">
+                <input type="email" name="newsletter_email" id="newsletterEmail" placeholder="Enter your email here">
+                <button class="btn btn-secondary submit-btn">Submit</button>
+            </form>
+        </div>
+        <img src="/wp-content/uploads/2023/10/newsletter-image.png" alt="Newsletter image">
+    </div>
+</div>
+
+<div class="faqs">
+    <div class="container faqs-container">
+        <h2>Got a question? Here you will find the answer.</h2>
+        <div class="accordion-wrapper">
+            <div class="item active">
+                <p class="title">Is this an online or live masterclass?</p>
+                <p class="content">Working internationally, Danilo and Sharon photographed weddings and portraits around the world, from the busy streets of Hong Kong to historic Paris.The studio is based in Belgrade and is led by wife and husband - Sharon and Danilo Vasic.</p>
+            </div>
+            <div class="item">
+                <p class="title">Is it pre-recorded content or a live presentation?</p>
+                <!-- <p class="content">Working internationally, Danilo and Sharon photographed weddings and portraits around the world, from the busy streets of Hong Kong to historic Paris.The studio is based in Belgrade and is led by wife and husband - Sharon and Danilo Vasic.</p> -->
+            </div>
+            <div class="item">
+                <p class="title">Who is this masterclass for?</p>
+                <!-- <p class="content">Working internationally, Danilo and Sharon photographed weddings and portraits around the world, from the busy streets of Hong Kong to historic Paris.The studio is based in Belgrade and is led by wife and husband - Sharon and Danilo Vasic.</p> -->
+            </div>
+            <div class="item">
+                <p class="title">When will it be available for watching?</p>
+                <!-- <p class="content">Working internationally, Danilo and Sharon photographed weddings and portraits around the world, from the busy streets of Hong Kong to historic Paris.The studio is based in Belgrade and is led by wife and husband - Sharon and Danilo Vasic.</p> -->
+            </div>
+            <div class="item">
+                <p class="title">What can I expect after I finish the masterclass?</p>
+                <!-- <p class="content">Working internationally, Danilo and Sharon photographed weddings and portraits around the world, from the busy streets of Hong Kong to historic Paris.The studio is based in Belgrade and is led by wife and husband - Sharon and Danilo Vasic.</p> -->
+            </div>
+        </div>
+        <p class="text-us-p">If you have any more questions, please don't hesistate to text us at info@reflections.com</p>
+    </div>
+</div>
+
+<div class="where-do-you-fit">
+    <div class="radial-background-1"></div>
+    <div class="radial-background-2"></div>
+    <div class="container where-do-you-fit-container">
+        <p class="subtitle">Courses</p>
+        <h2>Where do you  fit into the picture?</h2>
+        <div class="courses-wrapper">
+            <div class="course">
+                <h3>Wedding photography flash.</h3>
+                <p>An approachable way to learn web design & CSS in Squarespace, get up to speed on freelancing workflows (hourly rates, client contracts), and level up with 1on1 design critiques!</p>
+                <div class="buttons-wrapper">
+                    <a href="#" class="btn btn-primary">Enrollment Open</a>
+                    <a href="#" class="btn btn-secondary">Check more about</a>
+                </div>
+            </div>
+            <div class="course">
+                <h3>Lorem ipsum text comes here now.</h3>
+                <p>An approachable way to learn web design & CSS in Squarespace, get up to speed on freelancing workflows (hourly rates, client contracts), and level up with 1on1 design critiques!</p>
+                <div class="buttons-wrapper">
+                    <a href="#" class="btn btn-primary">Enrollment Open</a>
+                    <a href="#" class="btn btn-secondary">Check more about</a>
                 </div>
             </div>
         </div>
